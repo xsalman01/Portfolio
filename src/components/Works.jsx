ef6c06b096427e7d05c1e65f547498ff696ffc22
heisenberg <sasalman00@gmail.com>
2023-05-26 17:20:28 +0500

    eslint rules

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/prop-types */
 import { Tilt } from 'react-tilt';
 import { motion } from 'framer-motion';
 import { styles } from '../styles';
